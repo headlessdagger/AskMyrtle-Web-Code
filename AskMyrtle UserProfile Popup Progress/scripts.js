69bc6c7a4afe7769eb9c01adf0fc4b02e40ffc5a
Daniel Almiron <danielalmiron5450r@gmail.com>
2024-12-17 13:08:16 -0500

    Added more content to userprofile popup

--- AskMyrtle UserProfile Popup Progress/scripts.js
+++ AskMyrtle UserProfile Popup Progress/scripts.js
@@ -1,14 +1,15 @@
 const button1 = document.querySelector('.button-box');
-const popup = document.querySelector('.popup');
-const body = document.querySelector('body');
+const popup = document.querySelector('#profile-config-div');
+const main_div = document.querySelector('#main-div');
 const mask1 = document.querySelector('#mask1');
 const closeButton = document.querySelector('#closeX')
 
 function doStuff(){
     button1.classList.toggle('toggled1');
-    popup.classList.toggle('d-none')
-    mask1.classList.toggle('d-none')
-    mask1.classList.toggle('vignette')
+    popup.classList.toggle('d-none');
+    mask1.classList.toggle('d-none');
+    main_div.classList.toggle('blur');
+    // mask1.classList.toggle('vignette')
 }
 
 
