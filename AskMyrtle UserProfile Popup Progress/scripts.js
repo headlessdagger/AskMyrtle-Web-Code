6e30f6e0a0431c365af08e39c5ae7124f6ee35e8
Daniel Almiron <danielalmiron5450r@gmail.com>
2025-01-14 14:30:39 -0500

    Finished first section besides the password & secret phrase changing buttons

--- AskMyrtle UserProfile Popup Progress/scripts.js
+++ AskMyrtle UserProfile Popup Progress/scripts.js
@@ -3,8 +3,10 @@ const popup = document.querySelector('#profile-config-div');
 const main_div = document.querySelector('#main-div');
 const mask1 = document.querySelector('#mask1');
 const closeButton = document.querySelector('#closeX');
+const passwordChangeButton = document.querySelector("password-change-button");
+const secretPhraseChangeButton = document.querySelector("secret-phrase-change-button");
 
-function doStuff(){
+function openProfileWindow(){
     button1.classList.toggle('toggled1');
     popup.classList.toggle('d-none');
     mask1.classList.toggle('d-none');
@@ -12,6 +14,14 @@ function doStuff(){
     // mask1.classList.toggle('vignette')
 }
 
+function changeSecretPhraseWindow(){
+}
 
-button1.addEventListener('click', () => {doStuff()})
-closeButton.addEventListener('click', () => {doStuff()})
+function changePasswordWindow(){
+
+}
+
+button1.addEventListener('click', () => {openProfileWindow()})
+closeButton.addEventListener('click', () => {openProfileWindow()})
+passwordChangeButton.addEventListener('click', () => {changePasswordWindow()})
+secretPhraseChangeButton.addEventListener('click', () => {changeSecretPhraseWindow()})
